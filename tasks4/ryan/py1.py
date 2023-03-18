77e5165516bdd85c8a5a972b3a2842daf0b58d1e
Giggs471 <blitzwizard45@gmail.com>
2023-03-18 15:43:58 -0700

    ryan

--- tasks4/ryan/py1.py
+++ tasks4/ryan/py1.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 url = "https://en.wikipedia.org/wiki/List_of_open_government_data_sites"
 headers = {}  #To get header; google search 'what is my user agent'
@@ -15,8 +16,31 @@ with open("output1.html", "w",encoding='utf8') as file:
     file.write(str(soup))
     
 tab = soup.find_all("table",{"class":"wikitable"})
-print(tab[1])
+#list of tables 
 
-
+# this is just for analysis will be removed later
 with open("output3.html", "w",encoding='utf8') as file:
     file.write(str(tab[1]))
+
+og_tabl1 = tab[1]
+#getting the second table 
+
+#creating csv to add our values
+fo = open("result.csv","w",newline="")
+csvwriter =  csv.writer(fo)
+#calling writerow to add our headers
+csvwriter.writerow((["government","website","portal_name"]))
+#iterating over table values as td
+for row in og_tabl1.tbody.find_all('tr'):
+    #find all data fro each column
+    columns = row.find_all('td')
+    if (columns != []):
+        government = columns[0].text.strip()
+        website = columns[1].text.strip()
+        portal_name = columns[2].text.strip()
+        #creating result.csv 
+        #adding values
+        csvwriter.writerow((government,website,portal_name))
+
+
+
